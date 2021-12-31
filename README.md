663b6f64d4907fba19808d6476ae7098581fe9b4
Draperaj <draperaj@gmail.com>
2021-12-31 12:36:20 -0400

    more basic database setup
    added / upgraded the customer table, inventory table, and address table in the setup file. Created a docker-compose to start up the database container with the setup file. there is some base customer data and inventory data in the setup file. added query to look up customer info in the readme.

---
+++ README.md
@@ -0,0 +1,13 @@
+# jaedenar
+
+
+
+## Useful DB Queries
+
+### User Info Lookup
+```sql
+SELECT c.id as id, CONCAT(c.first_name, ' ', c.last_name) as name, a.street as street, a.city as city, a.state as state, a.zip as zip, a.country as country
+FROM customer c
+JOIN address a
+ON c.id = a.customer_id
+```
---
+++ db/setup.sql
@@ -0,0 +1,73 @@
+-- Customer Table
+CREATE TABLE IF NOT EXISTS `customer` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- Inventory Table
+CREATE TABLE IF NOT EXISTS `inventory` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) not null,
+    `price` decimal(15,2) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- Address Table, used for storing customer's address data
+CREATE TABLE IF NOT EXISTS `address` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `customer_id` INT NOT NULL,
+    `street` varchar(255) not null,
+    `city` varchar(255) not null,
+    `state` varchar(255) not null,
+    `zip` varchar(255) not null,
+    `country` varchar(255) not null,
+    PRIMARY KEY (`id`),
+    CONSTRAINT FK_customer_id FOREIGN KEY (`customer_id`) REFERENCES customer(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- customer base and addresses
+
+INSERT INTO customer
+VALUES (NULL, "draperaj", "Alex", "Draper");
+INSERT INTO address
+VALUES (NULL, 1, "223 Queen St.", "Charlottetown", "PE", "C1A9B2", "CA");
+
+INSERT INTO customer
+VALUES (NULL, "bobmct", "Bob", "McTest");
+INSERT INTO address
+VALUES (NULL, 2, "845 Avison Way", "Vancouver", "BC", "V6G3E2", "CA");
+
+INSERT INTO customer
+VALUES (NULL, "sam", "Sam", "MacTest");
+INSERT INTO address
+VALUES (NULL, 3, "1435 Brickell Avenue", "Miami", "FL", "33132", "US");
+
+INSERT INTO customer
+VALUES (NULL, "martha", "Martha", "Supertester");
+INSERT INTO address
+VALUES (NULL, 4, "1060 W Addison St.", "Chicago", "IL", "60613", "US");
+
+
+
+-- used for the base inventory provided for the application
+
+INSERT INTO inventory
+VALUES (NULL, "Grapes", 10.00);
+
+INSERT INTO inventory
+VALUES (NULL, "Strawberries", 12.50);
+
+INSERT INTO inventory
+VALUES (NULL, "Orange", 20.40);
+
+INSERT INTO inventory
+VALUES (NULL, "Banana", 29.99);
+
+INSERT INTO inventory
+VALUES (NULL, "Mango", 54.99);
+
+INSERT INTO inventory
+VALUES (NULL, "Apple", 100.00);
