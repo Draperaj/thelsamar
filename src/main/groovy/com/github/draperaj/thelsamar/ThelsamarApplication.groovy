794e3ff4d8f1d559da37a5e797216257861e47e7
Draperaj <draperaj@gmail.com>
2021-12-31 12:43:03 -0400

    added base spring boot project

---
+++ src/main/groovy/com/github/draperaj/thelsamar/ThelsamarApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.draperaj.thelsamar
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ThelsamarApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ThelsamarApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/github/draperaj/thelsamar/ThelsamarApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.github.draperaj.thelsamar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThelsamarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
